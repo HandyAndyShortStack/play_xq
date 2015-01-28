f593d8867844bdbe001e3da242e0f95148011247
Andy Short <andynshort@gmail.com>
2015-01-28 12:51:16 -0800

    basic build environment set up

---
+++ server.js
@@ -0,0 +1,17 @@
+var nodeStatic = require('node-static');
+var cowsay = require('cowsay');
+
+var file = new nodeStatic.Server('./html');
+var port = 4010;
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
+}).listen(port);
+
+console.log(cowsay.say({
+  text: 'Your server is up and running on port ' + port + ', boss!',
+  e: 'oO',
+  T: 'U '
+}));
